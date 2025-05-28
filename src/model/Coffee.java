af2423317cb289d13b5381e6903d5cd5cacafcb9
Salohyy212 <zerambaysalohy@gmail.com>
2025-05-28 09:15:03 +0300

    feat: implement coffee model

---
+++ src/model/Coffee.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Coffee {
+    private final String name;
+    private final double price;
+    private int stock;
+
+    public Coffee(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getStock() { return stock; }
+
+    public void reductionStock() {
+        if (stock > 0) stock--;
+    }
+
+    public boolean disponibleStock() {
+        return stock > 0;
+    }
+}
